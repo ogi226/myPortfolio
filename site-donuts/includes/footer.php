27ab1aa22f3cf2cfff096312953193b09dc0ab32
ogi226 <158382343+ogi226@users.noreply.github.com>
2024-04-29 14:19:32 +0900

    Add files via upload

---
+++ site-donuts/includes/footer.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="common/css/reset.css">
+  <link rel="stylesheet" href="common/css/common.css">
+  <link rel="stylesheet" href="common/css/footer.css">
+
+  <!-- <title>header確認</title> -->
+
+</head>
+
+<body>
+<footer>
+
+
+    <div id=footer_content>
+        <div id=footer_nav>
+            <a href="#"><p>よくある質問</p></a>
+            <a href="#"><p>お問い合わせ</p></a>
+            <a href="#"><p>当サイトのポリシー</p></a>
+        </div>
+
+        <ul id=footer_sns>
+           <li><a href="#"><img src="common/images/footer_Instagram2_sp.png" alt="インスタンス"></a></li>
+            <li><a href="#"><img src="common/images/footer_facebook_sp.png" alt="フェイスブック"></a></li>
+            <li><a href="#"><img id="x" src="common/images/footer_twitterX_sp.png" alt="エックス"></a></li>
+        </ul>
+
+        <div id=footer_logo>
+            <a href="index.php"><img src="common/images/logo_sp.png" alt="C.C.Donuts画像"></a>
+        </div>
+
+
+
+    <div id=footer_copyright>
+    <p>Copyright (C) c.c.donuts All Rights Reserved</p>
+    </div>
+</div>
+
+</footer>
+
+
+</body>
+</html>
